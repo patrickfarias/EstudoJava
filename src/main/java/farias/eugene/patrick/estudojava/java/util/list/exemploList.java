05f9bee7d9d5c90f5393cced74bb099a36f20a78
“patrickfarias” <patrickfarias@icloud.com>
2020-10-16 20:19:16 -0300

    Exemplo List

---
+++ src/main/java/farias/eugene/patrick/estudojava/java/util/list/exemploList.java
@@ -0,0 +1,9 @@
+package farias.eugene.patrick.estudojava.java.util.list;
+
+public class exemploList {
+
+    public static void main(String[] args) {
+        System.out.println("ola");
+    }
+
+}
