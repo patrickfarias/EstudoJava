a154d78d3e657093641257664455c4128ed3434d
“patrickfarias” <patrickfarias@icloud.com>
2020-10-16 19:59:21 -0300

    Primeiro Commit

---
+++ src/main/java/farias/eugene/patrick/estudojava/EstudojavaApplication.java
@@ -0,0 +1,13 @@
+package farias.eugene.patrick.estudojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudojavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/farias/eugene/patrick/estudojava/EstudojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package farias.eugene.patrick.estudojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
